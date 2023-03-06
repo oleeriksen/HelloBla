789d885a49da3e4a17bb854858c5b4008b4ea3c3
oleeriksen <oleeriksen1958@gmail.com>
2023-03-06 14:29:31 +0100

    product v1

---
+++ HelloBla/Client/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloBla.Client.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name must be less than 50 characters.")]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsPublished { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+    }
+}
+
